2df655e2467ceb4bdbea6134455c6f187e1cef88
Akmal Adnan <81947525+akmal-adnan@users.noreply.github.com>
2025-07-29 19:27:45 +0800

    feat: removed unused code

--- src/components/Posts/styles.module.scss.d.ts
+++ src/components/Posts/styles.module.scss.d.ts
@@ -1,9 +1,2 @@
-export declare const buttonSourceCode: string;
-export declare const cardContainer: string;
-export declare const cardImageContainer: string;
-export declare const cardItem: string;
-export declare const cardTextContainer: string;
-export declare const cardTitleGroup: string;
-export declare const centerContainer: string;
 export declare const sectionContainer: string;
 export declare const titleContainer: string;
